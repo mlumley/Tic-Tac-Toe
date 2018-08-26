90309a27b01c6f882b7e086f3fc8ea9915e9af3a
Michael Lumley <mlumley@student.unimelb.edu.au>
2018-08-26 14:37:00 +1000

    Moved UserInput to it's own class

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,33 +1,9 @@
-from Coordinate import Coordinate
 from Player import Player
 from Board import Board
+from UserInput import UserInput
 from Error import Error
-from Error import InvalidCoordinateLengthError
 
 BOARD_SIZE = 3
-
-def processUserInput(player):
-    """
-    Get player input from the terminal and return a coordinate
-    corispnding to the move or exit the game if the player
-    chose to quit.
-    """
-    action = input("Player " + player.id + " enter a coord x,y to place your " + player.symbol + " or enter 'q' to give up: ")
-
-    if action == 'q':
-        print("Exiting")
-        exit()
-
-    action = action.split(',')
-
-    if len(action) != 2:
-        raise InvalidCoordinateLengthError("Error: Invalid coordinate length. Coordinates should be of the format x,y")
-    else:
-        coord = Coordinate(action[0], action[1])
-        # Convert coordinate from 1 based to 0 based
-        coord.subtract(1)
-        return coord
-
 
 def main():
     """
@@ -39,12 +15,14 @@ def main():
     player2 = Player('2', 'O')
     currentPlayer = player1
 
+    userInput = UserInput()
+
     print("Welcome to Tic Tac Toe!")
     board.printBoard()
 
     while(True):
         try:
-            coord = processUserInput(currentPlayer)
+            coord = userInput.processUserInput(currentPlayer)
             board.markBoard(currentPlayer, coord)
             board.printBoard()
         except Error as error:
---
+++ UserInput.py
@@ -0,0 +1,25 @@
+from Coordinate import Coordinate
+from Error import InvalidCoordinateLengthError
+
+class UserInput():
+    def processUserInput(self, player):
+        """
+        Get player input from the terminal and return a coordinate
+        corresponding to the move or exit the game if the player
+        chose to quit.
+        """
+        action = input("Player " + player.id + " enter a coord x,y to place your " + player.symbol + " or enter 'q' to give up: ")
+
+        if action == 'q':
+            print("Exiting")
+            exit()
+
+        action = action.split(',')
+
+        if len(action) != 2:
+            raise InvalidCoordinateLengthError("Error: Invalid coordinate length. Coordinates should be of the format x,y")
+        else:
+            coord = Coordinate(action[0], action[1])
+            # Convert coordinate from 1 based to 0 based
+            coord.subtract(1)
+            return coord
